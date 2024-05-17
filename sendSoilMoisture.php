ed15c8077a6f7aa9c6dae13baa7099517cb5fa4a
kokojimz <reynalldymarchell03@gmail.com>
2024-05-17 15:38:35 +0700

    fix: update data in database from sensor

--- sendSoilMoisture.php
+++ sendSoilMoisture.php
@@ -3,7 +3,5 @@
 $conn = mysqli_connect("localhost", "root", "", "websensorsoil");
 $value = $_GET['sensor'];
 
-if ($value >= 20) {
-    //update data in database from sensor
-    mysqli_query($conn, "INSERT INTO sensor_sm(nilai_sensor) VALUES('$value')");
-}
+//update data in database from sensor
+mysqli_query($conn, "INSERT INTO sensor_sm(nilai_sensor) VALUES('$value')");
